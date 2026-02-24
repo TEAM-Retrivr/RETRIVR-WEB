ac426003dbdaacc8c43aa52af2c5c6f37dfcff09
joshcho <sungho020426@naver.com>
2026-02-24 16:46:42 +0900

    Setting, Designfix: index.css  컴포넌트 텍스트 크기 상수화 및 메뉴 카드 컴포넌트에 적용

--- src/components/cards/home/RentRequestCard.tsx
+++ src/components/cards/home/RentRequestCard.tsx
@@ -14,16 +14,14 @@ export const RentRequestCard = ({
   time,
 }: RentRequestCardProps) => {
   return (
-    <div className="w-full max-w-[318px] max-h-[94px] bg-neutral-white rounded-[20px] p-5 shadow-sm border border-neutral-gray/20 flex justify-between items-center">
+    <div className="w-full max-w-[318px] max-h-[94px] bg-neutral-white rounded-[20px] p-5 shadow-sm border border-neutral-gray-2/20 flex justify-between items-center">
       <div>
         <div className="flex items-center gap-1 leading-none">
-          <h3 className="font-[600] text-[1rem]">{itemName}</h3>
-          <span className="text-secondary-light text-[12px]">{count}</span>
+          <h3 className="font-[600] text-16px">{itemName}</h3>
+          <span className="text-secondary-3 text-[12px]">{count}</span>
         </div>
-        <p className="text-[0.75rem] text-neutral-dark mt-1">{applicant}</p>
-        <p className="text-[0.625rem] text-[#9c9c9c] font-normal mt-1">
-          {time}
-        </p>
+        <p className="text-12px text-neutral-gray-1 mt-1">{applicant}</p>
+        <p className="text-10px text-neutral-gray-3 font-normal mt-1">{time}</p>
       </div>
       {/* TODO : 확인하기 버튼 클릭 시 세부사항 페이지로 라우팅 */}
       <Button size="xs">확인하기</Button>
