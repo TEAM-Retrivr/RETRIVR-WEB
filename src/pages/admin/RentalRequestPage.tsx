ab9102cd58d1743e0b15af2236d6ecc454a14c3a
joshcho <sungho020426@naver.com>
2026-02-28 20:18:13 +0900

    design: 대여 요청 확인 페이지 1차 구현

---
+++ src/pages/admin/RentalRequestPage.tsx
@@ -0,0 +1,29 @@
+import { Layout } from "../../components/Layout";
+import Header from "../../components/Header";
+import RentalConfirmCard from "../../components/cards/RentalConfirmCard";
+
+const sampleRental: RentalRequest = {
+  requestedAt: "2026-01-21 17:00",
+  itemName: "c타입 충전기",
+  itemId: "c타입 충전기 (1)",
+  itemCount: "(2/5)",
+  applicantInfo: {
+    name: "조윤아",
+    major: "동물자원학과",
+    id: "202312690",
+  },
+};
+
+const RentalRequestPage = () => {
+  return (
+    <Layout>
+      <Header name="건국대학교 도서관자치위원회" pageName="대여요청"></Header>
+      <div className="flex flex-col w-full items-center mt-8.5 gap-5">
+        <RentalConfirmCard rental={sampleRental}></RentalConfirmCard>
+        <RentalConfirmCard rental={sampleRental}></RentalConfirmCard>
+      </div>
+    </Layout>
+  );
+};
+
+export default RentalRequestPage;
