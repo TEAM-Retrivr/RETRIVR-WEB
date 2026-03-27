b43aae9c054ed48fba68d4c85241a5242266842b
joshcho <sungho020426@naver.com>
2026-03-27 21:27:18 +0900

    Edit : App.tsx 라우터 통일

--- src/App.tsx
+++ src/App.tsx
@@ -19,59 +19,46 @@ function App() {
       <Routes>
         {/* 랜딩 페이지 */}
         <Route path="/" element={<LandingPage />} />
-      </Routes>
-      <Routes>
+
         {/* 로그인 페이지 */}
         <Route path="/login" element={<LoginPage />} />
-      </Routes>
-      <Routes>
+
         {/* 회원가입 페이지 */}
         <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-      <Routes>
+
         {/* 관리자 홈 페이지 */}
         <Route path="/home" element={<HomePage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여자 물품 관리 페이지 */}
         <Route path="/item-manage" element={<ItemManagementPage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여자 물품 신규 등록 페이지 */}
         <Route path="/item-register" element={<ItemRegisterationPage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여자 반납 관리 페이지 */}
         <Route path="/return-manage" element={<ReturnManagementPage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여자 물품별 관리 페이지 */}
         <Route path="/return-check/:itemId" element={<ReturnCheckPage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여 요청 확인 페이지 */}
         <Route path="/rental-request" element={<RentalRequestPage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여자 대여지 검색 페이지 */}
         <Route path="/client-search" element={<RenterSearchPage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여자 홈 페이지 */}
         <Route path="/client-home" element={<ClientHomePage />} />
-      </Routes>
-      <Routes>
+
         {/* 대여자 대여 신청 페이지 */}
         <Route
           path="/client-rental-information-submit"
           element={<RentalInformationSubmitPage />}
         />
-      </Routes>
-      <Routes>
+
         {/* 테스트 페이지 */}
         <Route path="/test" element={<TestPage />} />
-      </Routes>
-      <Routes>
+
         {/* 테스트 페이지 */}
         <Route path="/modal-test" element={<ModalTestPage />} />
       </Routes>
