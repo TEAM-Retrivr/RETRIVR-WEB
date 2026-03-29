85d4a5d94377ef06b5b345298f01515b514b71f1
joshcho <sungho020426@naver.com>
2026-03-30 01:59:26 +0900

    Edit, Delete: 프로필 기본 아이콘 이미지 통일 및 검색용 아이콘 삭제

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -9,19 +9,20 @@ const UserIcon = ({
   usage = "home",
   alt = "프로필 이미지",
 }: UserIconProps) => {
-  const iconSize =
+  const iconBoxSize =
     usage === "home"
       ? "w-18 h-18 border border-neutral-gray-4 border-[0.6px] shadow-profile"
-      : "w-14 h-14 pt-2 shadow-[0_0_3px_1px_rgba(92,174,255,0.4)]";
-  const defaultProfileImage =
-    usage === "home"
-      ? "/icons/default-profile.svg"
-      : "/icons/client/search-profile-default.svg";
-  const wrapperClass = `${iconSize} flex items-center justify-center rounded-full  bg-neutral-white  overflow-hidden`;
+      : "w-14 h-14  shadow-[0_0_3px_1px_rgba(92,174,255,0.4)]";
+  const iconSize = usage === "home" ? "w-10" : "w-7";
+  const wrapperClass = `${iconBoxSize} flex items-center justify-center rounded-full  bg-neutral-white pt-2  overflow-hidden`;
   if (!imageURL)
     return (
       <div className={wrapperClass}>
-        <img src={defaultProfileImage} alt="기본 프로필" />
+        <img
+          className={iconSize}
+          src="/icons/default-profile.svg"
+          alt="기본 프로필"
+        />
       </div>
     );
 
