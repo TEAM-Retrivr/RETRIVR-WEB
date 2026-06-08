e8e2a77fbaf6c22f0d78fb0f7950ddc2fbfa4845
joshcho <sungho020426@naver.com>
2026-06-08 16:39:31 +0900

    turn off: 멤버쉽 페이지 접근 비활성화

--- src/pages/admin/AccountPage.tsx
+++ src/pages/admin/AccountPage.tsx
@@ -215,7 +215,7 @@ const AccountPage = () => {
           <div className="flex flex-col w-full h-13.25 text-14px font-bold shadow-16-gray rounded-2xl">
             <button
               type="button"
-              onClick={() => navigate("/membership")}
+              onClick={() => alert("개발 예정입니다.")}
               className="flex items-center justify-between h-13.25 px-7.5 rounded-2xl cursor-pointer hover:bg-neutral-gray-4/50"
             >
               <p className="text-start text-primary">Retrivr 프로</p>
