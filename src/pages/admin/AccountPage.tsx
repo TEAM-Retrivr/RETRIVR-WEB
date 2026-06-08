405a879a1c234a503adb1b809255a14f89b169e1
joshcho <sungho020426@naver.com>
2026-06-08 16:41:38 +0900

    turn off: navigation 비활성화

--- src/pages/admin/AccountPage.tsx
+++ src/pages/admin/AccountPage.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Layout } from "../../components/Layout";
 import Header from "../../components/Header";
 import ConfirmModal from "../../components/modals/ConfirmModal";
